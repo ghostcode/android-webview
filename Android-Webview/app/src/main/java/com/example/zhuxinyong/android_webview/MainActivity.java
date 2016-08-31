1789599439f1de6ee452011456ae512e5de85057
zhuxinyong <ghostcode521@gmail.com>
2016-08-31 10:42:25 +0800

    add webview

--- Android-Webview/app/src/main/java/com/example/zhuxinyong/android_webview/MainActivity.java
+++ Android-Webview/app/src/main/java/com/example/zhuxinyong/android_webview/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.zhuxinyong.android_webview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mWebView = (WebView) findViewById(R.id.mwebview);
+
+        mWebView.setWebViewClient(new WebViewClient());
+
         mWebView.loadUrl("http://cn.bing.com/");
     }
 }
