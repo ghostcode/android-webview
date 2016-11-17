fc432e08d9ae52246bdf93057e883810e3a41062
zhuxinyong <ghostcode521@gmail.com>
2016-11-17 10:05:30 +0800

    add load & loadData

--- Android-Webview/app/src/main/java/com/example/zhuxinyong/android_webview/MainActivity.java
+++ Android-Webview/app/src/main/java/com/example/zhuxinyong/android_webview/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.zhuxinyong.android_webview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -12,12 +14,32 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         mWebView = (WebView) findViewById(R.id.mwebview);
+        WebSettings settings = mWebView.getSettings();
+        settings.setJavaScriptEnabled(true);
 
-        mWebView.setWebViewClient(new WebViewClient());
+        WebView.setWebContentsDebuggingEnabled(true);
 
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                Log.e("iwjw============:",url);
+
+                boolean should = super.shouldOverrideUrlLoading(view, url);
+
+                return should;
+            }
+        });
+
+//        mWebView.loadUrl("file:///android_asset/index.html");
         mWebView.loadUrl("http://cn.bing.com/");
+//        String summary = "<html><body><h1>this is iwjw哈哈</h1></body></html>";
+//        mWebView.loadData(summary,"text/html;charset=UTF-8",null);
+//        mWebView.loadDataWithBaseURL(null,summary,"text/html","UTF-8",null);
     }
+
 }
